a0804479682ecf47befa99596d008448bd318511
santiago mathiasen <santiagomathiasen@gmail.com>
2026-03-18 20:46:31 -0300

    fix: upgrade sw.js to force browser reload after clearing Vite cache

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,21 @@
-self.addEventListener('install', (e) => {
+// Self-destructing Service Worker - clears old Vite cache and reloads all tabs
+self.addEventListener('install', () => {
     self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-    e.waitUntil(
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
         caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    return caches.delete(cacheName);
-                })
-            );
+            return Promise.all(cacheNames.map((name) => caches.delete(name)));
         }).then(() => {
-            self.clients.claim();
-            self.registration.unregister();
+            return self.clients.claim();
+        }).then(() => {
+            // Tell all open tabs to reload so they pick up the new Next.js build
+            return self.clients.matchAll({ type: 'window' }).then(clients => {
+                clients.forEach(client => client.navigate(client.url));
+            });
+        }).then(() => {
+            return self.registration.unregister();
         })
     );
 });
